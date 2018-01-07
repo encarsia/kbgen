5c25f539e5a03906cd17f8f6a7b857275ecbb5ee
encarsia <An.Ke@bahnfreikartoffelbrei.de>
2018-01-07 13:18:40 +0100

    file upload

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# kbgen
-Kettlebattle generator (Flask app)
+##### (German version)
+
+## KETTLE...WAS?
+
+Genau. Okay, du weißt nicht, was Kettlebattle ist, kein Problem. Die Menschen, die diesem Unsinn nachgehen, dürften sich zahlenmäßig aktuell im einstelligen Bereich bewegen (Stand: Januar 2018).
+
+**Kettlebattle ist ein kollaboratives Kettlebell-Home-Workout ab zwei Mitspielern.**
+
+Die Frontlinie der "Battle" befindet sich dabei nicht zwischen den Mitspielern, sondern zwischen Motivation, Schweinehund, Sofa und Hanteln. Team up!
+
+## SPIELREGELN
+
+1. Die Kontaktaufnahme  
+Der potentielle Mitspieler wird mit *"Kettlebattle?"* kontaktiert.
+1. Der Workout-Vorschlag  
+Hat der Mitspieler die Kontaktaufnahme positiv beantwortet, schlägt der Initiator ein Workout vor, das der Mitspieler akzeptieren muss. Fairness geht vor. Wer nach einem langen Tag mitten in der Nacht seinem Mitspieler ein hartes Workout vorschlägt, hat schnell keine Mitspieler mehr.
+1. Do you even lift?  
+Do it.
+1. Das Finisher-Selfie  
+Der krönende Abschluss jeder Kettlebattle ist das Finisher-Foto, das man dem Mitspieler nach dem Workout als Beweis seiner Sportlichkeit zuschickt. Hierbei ist absolute Ernsthaftigkeit zu wahren, es geht hier schließlich um Kettlebattle!
+
+## DER GENERATOR
+
+Der Kettlebattle-Generator ist eine Einseiten-Web-App, mit der sich aus einem zuvor definierten Übungspool ein Workout generieren lässt.
+
+### Habenwill!
+
+* für die Ausführung wird [Flask](http://flask.pocoo.org) benötigt
+* `./kbgen.py` ausführen, dies startet den internen Flask-Server¹
+* [127.0.0.1:5000](http://127.0.0.1:5000/) aufrufen
+* Workout generieren
+* Werte können per Spinner geändert werden
+
+### Konfiguration
+
+In der `./kbgen.py` gibt es 3 Variablen für individuelle Einstellungen:
+
+1. **`REPS_PRESET`**: Tupel mit 2 Werten (int) für die Preset-Buttons, Gesamtwiederholungen 
+1. **`KB_EX`**: Liste von Übungen, die bei der Kettlebattle zur Auswahl stehen sollen, ein Listeneintrag ist ein Tupel nach dem Muster (Name, Minimum bei Preset 1, Minimum bei Preset 2)
+1. **`MAX_EX`**: Maximale Anzahl (int) von Übungen, die aus der Liste durchgeführt werden sollen, Minimumwerte werden immer gezählt
+
+## Verweise
+
+* Kettlebell-Image: [openclipart.org](https://openclipart.org/detail/241218/kettlebell)
+* Layout: [An Introduction to Python’s Flask Framework](https://code.tutsplus.com/tutorials/an-introduction-to-pythons-flask-framework--net-28822)
+* Kettlebattle ist eine Erfindung von [ZeEvilKohl](https://twitter.com/ZeEvilKohl) und [encarsia_](https://twitter.com/encarsia_) (yours truly)
+
+##### (English version)
+## KETTLE...WHAT?
+
+to soon
+_____
+
+¹ wie man Flask-Apps produktiv betreibt, erfährt man u.a. in der [Dokumentation](http://flask.pocoo.org/docs/0.12/deploying/)
+
+
---
+++ kbgen.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import markdown
+import random
+from flask import Flask, render_template, request, Markup
+
+############# KETTLEBATTLE STATION ##############
+
+REPS_PRESET = (200, 300)    #set absolute repetitions for preset buttons
+
+# kettlebell exercises (name, min at preset 1, min at preset 2)
+KB_EX = [("Squats (w/o)", 50, 50),
+         ("Squats (w/)", 0, 0),
+         ("Swings (one arm)", 50, 100),
+         ("Swings (two arms)", 0, 0),
+         ("Snatches", 0, 40),
+         ("Clean & Jerks", 0, 0),
+         ("Rows", 0, 0),
+         ("Figure-8 curls", 0, 0),
+         ("Halos", 0, 20),
+         ("Deadlifts", 0, 0),
+         ("Kettlebell circles", 0, 30),
+]
+
+MAX_EX = 4  #max number of exercises to generate workout, minimum presets are always counted
+
+####################################################
+
+FORM_PRESET = [("{} {}".format(REPS_PRESET[0], 1), REPS_PRESET[0]),
+               ("{} {}".format(REPS_PRESET[1], 2), REPS_PRESET[1])]
+
+
+def generate_workout(abs_reps, pos):
+    #generate exercise list for chosen preset
+    exercises = []
+    for ex in KB_EX:
+        exercises.append([ex[0], ex[pos]])
+    #sum up given minimum values
+    min_reps = 0
+    for ex in exercises:
+        min_reps += ex[1]
+    #choose random exercises from list, max number given in MAX_EX variable
+    dothese = random.sample(exercises, MAX_EX)
+    #add 10 repetitions to one of the random exercises to absolute repetition number
+    for x in range(min_reps, abs_reps, 10):
+        random.choice(dothese)[1] += 10
+    return exercises
+
+########### FLASK ###########
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form['submit'] == 'generate':
+            #create workout list, render template with this list instead of KB_EX so values are kept
+            workout = []
+            for ex in KB_EX:
+                workout.append((ex[0], int(request.form[ex[0]])))
+            #create list for sharing via c&p, don't list exercises w/ 0 reps
+            battle = []
+            total = 0
+            for ex in workout:
+                if ex[1] > 0:
+                    battle.append((ex[0], ex[1]))
+                    total += ex[1]
+            return render_template('kettlebattle.html', form=request.form, reps=FORM_PRESET, exc=workout, battletext=battle, total=total)
+        elif request.form['submit']:
+            abs_reps, pos = request.form['submit'].split()
+            workout = generate_workout(int(abs_reps), int(pos))
+            return render_template('kettlebattle.html', form=request.form, reps=FORM_PRESET, exc=workout)
+    elif request.method == 'GET':
+        return render_template('kettlebattle.html', form=request.form, reps=FORM_PRESET, exc=KB_EX)
+
+@app.route('/about')
+def about():
+    with open('README.md') as f:
+        readme = f.read()
+    readme = Markup(markdown.markdown(readme))
+    return render_template('about.html', readme=readme)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
